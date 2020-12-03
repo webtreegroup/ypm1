4a84c728e00246b0e923c4ab1db3c2c2e49880c6
web-tree <web-tree@yandex.ru>
2020-12-03 10:35:24 +0400

    декомпозиция чата на компоненты

--- server.js
+++ server.js
@@ -8,14 +8,20 @@ app.set('view engine', 'pug')
 app.use(express.static(`${__dirname}/static`))
 
 app.get("/login", function(request, response){
-    response.render("pages/login", {
-        title: "login page"
+    response.render("pages/Login", {
+        title: "Login page"
     })
 })
 
 app.get("/signin", function(request, response){
-    response.render("pages/signin", {
-        title: "signin page"
+    response.render("pages/Signin", {
+        title: "Signin page"
+    })
+})
+
+app.get("/chats", function(request, response){
+    response.render("pages/Chats", {
+        title: "Chats page"
     })
 })
 
