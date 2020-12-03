05abc058eb7344cbeb4d0ce5e73c89647bc356cf
web-tree <web-tree@yandex.ru>
2020-12-03 22:22:59 +0400

    поставил typescript, настроил tsconfig, поменял структуру проекта

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+const { getChatsData } = require('./static/js/mocks/chats.mock')
+
 const express = require('express')
 
 const app = express()
 const PORT = 4000
 
 app.set('view engine', 'pug')
+app.set('views', './src')
 
 app.use(express.static(`${__dirname}/static`))
 
@@ -21,7 +24,16 @@ app.get("/signin", function(request, response){
 
 app.get("/chats", function(request, response){
     response.render("pages/Chats", {
-        title: "Chats page"
+        title: "Chats page",
+        chatsData: getChatsData()
+    })
+})
+
+app.get("/chats/:id", function(request, response){
+    response.render("pages/ChatSingle", {
+        title: "Chats page",
+        chatsData: getChatsData(true),
+        chatMessages: 'TODO'
     })
 })
 
---
+++ src/mocks/chats.mock.ts
@@ -0,0 +1,23 @@
+const getChatsData = (isCurrent: boolean) => [
+    { 
+        name: 'Андрей', 
+        lastMessage: 'И Human Interface Guidelines и Material Design рекомендуют...', 
+        time: '10:49', 
+        unread: 4 
+    },
+    { 
+        name: 'Вадим', 
+        lastMessage: 'Круто!', 
+        time: 'Пт', 
+        isOwnMessage: true,
+        isCurrent: isCurrent
+    },
+    { 
+        name: 'Day.', 
+        lastMessage: 'Так увлёкся работой по курсу, что совсем забыл его анонсир...', 
+        time: '1 Мая 2020', 
+        unread: 100, 
+    },
+]
+
+module.exports = { getChatsData }
